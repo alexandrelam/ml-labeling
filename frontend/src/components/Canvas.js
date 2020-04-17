d85daaab31a7a9134b3e987f1458785fd768992f
Alexandre LAM <mrnivclones@gmail.com>
2020-04-17 19:02:54 +0200

    Added Rect ! On peut déplacer des rects et modifier leurs tailles Ajout de boutons qui ne fonctionnent pas

--- frontend/src/components/Canvas.js
+++ frontend/src/components/Canvas.js
@@ -1,16 +1,73 @@
 import React from "react";
-import { Layout } from "antd";
+import { Layout, Button } from "antd";
+import { Stage, Layer } from "react-konva";
+import "../App.css";
+import Rectangle from "./Rectangle";
 
 const { Content } = Layout;
 
-class Canvas extends React.Component {
-	render() {
-		return (
-			<Content className="site-layout-background">
-				<canvas>hello</canvas>
-			</Content>
-		);
-	}
-}
+const initialRectangles = [
+	{
+		x: 10,
+		y: 10,
+		width: 100,
+		height: 100,
+		id: "rect1",
+	},
+	{
+		x: 150,
+		y: 150,
+		width: 100,
+		height: 100,
+		id: "rect2",
+	},
+];
+
+const Canvas = () => {
+	const [rectangles, setRectangles] = React.useState(initialRectangles);
+	const [selectedId, selectShape] = React.useState(null);
+
+	const checkDeselect = (e) => {
+		const clickedOnEmpty = e.target === e.target.getStage();
+		if (clickedOnEmpty) {
+			selectShape(null);
+		}
+	};
+
+	return (
+		<Content>
+			<Stage
+				width={600}
+				height={600}
+				onMouseDown={checkDeselect}
+				onTouchStart={checkDeselect}
+			>
+				<Layer>
+					{rectangles.map((rect, i) => {
+						return (
+							<Rectangle
+								key={i}
+								shapeProps={rect}
+								isSelected={rect.id === selectedId}
+								onSelect={() => {
+									selectShape(rect.id);
+								}}
+								onChange={(newAttrs) => {
+									const rects = rectangles.slice();
+									rects[i] = newAttrs;
+									setRectangles(rects);
+								}}
+							/>
+						);
+					})}
+				</Layer>
+			</Stage>
+			<div className="btn-section">
+				<Button type="primary">Add rect</Button>
+				<Button danger>Remove rect</Button>
+			</div>
+		</Content>
+	);
+};
 
 export default Canvas;
