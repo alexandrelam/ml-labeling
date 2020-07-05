d27729b298ea143b74751b39ced95194b0d8200e
Alexandre LAM <alexandrelam@outlook.com>
2020-07-05 12:48:08 +0200

    Traduction des boutons et ajout de la notification lorsqu'on report

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,28 +1,29 @@
 import React, { useState } from "react";
-import { Modal, Layout, Button, Input } from "antd";
+import { Modal, Layout, Button, Input, Alert } from "antd";
 const { TextArea } = Input;
 
 function Sidebar(props) {
-	const { Sider } = Layout;
-	const [visible, setVisible] = useState(false);
-	const [loading, setLoading] = useState(false);
-	const [reportTxt, setReportTxt] = useState("");
+    const { Sider } = Layout;
+    const [visible, setVisible] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [reportTxt, setReportTxt] = useState("");
+    const [alert, setAlert] = useState(false);
 
-	const showModal = () => {
-		setVisible(true);
-	};
+    const showModal = () => {
+        setVisible(true);
+    };
 
-	const handleCancel = () => {
-		setVisible(false);
-	};
+    const handleCancel = () => {
+        setVisible(false);
+    };
 
-	const postReport = (e) => {
-		setLoading(true);
+    const postReport = (e) => {
+        setLoading(true);
 
-		let formData = new FormData();
-		formData.append("reportMsg", reportTxt);
-		formData.append("issue", props.issueList[props.pagination].id);
-		
+        let formData = new FormData();
+        formData.append("reportMsg", reportTxt);
+        formData.append("issue", props.issueList[props.pagination].id);
+
         fetch(
             "http://127.0.0.1:8000/report/",
 
@@ -34,54 +35,74 @@ function Sidebar(props) {
                 }),
                 body: formData,
             }
-		)
-		.then(setLoading(false))
-		.then(setVisible(false));
+        )
+            .then(setAlert(true))
+            .then(
+                setTimeout(() => {
+                    setAlert(false);
+                    setVisible(false);
+                    setLoading(false);
+                }, 2000)
+            );
     };
 
-	return (
-		<Sider width={300} className="site-layout-background">
-			<div className="description">
-				<h3>Description</h3>
-				{props.issueList.length !== 0 && (
-					<p>{Object.values(props.issueList[props.pagination])[2]}</p>
-				)}
-				<Button type="default" onClick={props.precedent}>
+    return (
+        <Sider width={300} className="site-layout-background">
+            <div className="description">
+                <h3>Description</h3>
+                {props.issueList.length !== 0 && (
+                    <p>{Object.values(props.issueList[props.pagination])[2]}</p>
+                )}
+                <Button type="default" onClick={props.precedent}>
                     Précédent
                 </Button>
                 <Button type="default" onClick={props.suivant}>
                     Suivant
                 </Button>
-				<Button type="primary" onClick={props.save}>Envoyer</Button>
-				<Button type="danger" onClick={showModal}>
-					Signaler
-				</Button>
-				<Button type="default" onClick={props.deleteAll}>
-					Tout supprimer
-				</Button>
-			</div>
-			<Modal
-				visible={visible}
-				title="Décrivez l'erreur"
-				onOk={postReport}
-				onCancel={handleCancel}
-				footer={[
-					<Button key="retour" onClick={handleCancel}>
-						Return
-					</Button>,
-					<Button key="envoyer" type="primary" loading={loading} onClick={postReport}>
-						Submit
-					</Button>
-				]}
-			>
-				<TextArea
-					placeholder="280 caractères maximum"
-					onChange={e=>setReportTxt(e.target.value)}
-					autoSize={{ minRows: 6, maxRows: 6 }}
-				/>
-			</Modal>
-		</Sider>
-	);
+                <Button type="primary" onClick={props.save}>
+                    Envoyer
+                </Button>
+                <Button type="danger" onClick={showModal}>
+                    Signaler
+                </Button>
+                <Button type="default" onClick={props.deleteAll}>
+                    Tout supprimer
+                </Button>
+            </div>
+            <Modal
+                visible={visible}
+                title="Décrivez l'erreur"
+                onOk={postReport}
+                onCancel={handleCancel}
+                footer={[
+                    <Button key="retour" onClick={handleCancel}>
+                        Retour
+                    </Button>,
+                    <Button
+                        key="envoyer"
+                        type="primary"
+                        loading={loading}
+                        onClick={postReport}
+                    >
+                        Envoyer
+                    </Button>,
+                ]}
+            >
+                {alert && (
+                    <Alert
+                        message="Merci de votre feedback !"
+                        type="success"
+                        showIcon
+                    />
+                )}
+                <TextArea
+                    placeholder="280 caractères maximum"
+                    onChange={(e) => setReportTxt(e.target.value)}
+                    autoSize={{ minRows: 6, maxRows: 6 }}
+                />
+            </Modal>
+        </Sider>
+    );
 }
 
 export default Sidebar;
